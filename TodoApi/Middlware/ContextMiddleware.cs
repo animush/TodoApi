d229f3ada7abecc4064d730c4416f470182bbd81
okostylenko <okostylenko@morebis.net>
2022-10-04 12:39:48 +0300

    Add User context Middlware

---
+++ TodoApi/Middlware/ContextMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using ToDo.Services;
+
+namespace TodoApi.Middlware
+{
+    public class ContextMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ContextMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, UserContext userContext)
+        {
+            var claim = context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.UserData);
+            if(claim != null && int.TryParse(claim.Value, out var userId))
+            {
+                userContext.CurrentUserId =  userId;
+            }
+
+            await _next(context);
+        }
+}
+
+    public static class ContextMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseContextMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ContextMiddleware>();
+        }
+    }
+}
